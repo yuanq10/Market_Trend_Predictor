cff9ac6afa2dd00878fd98e7af7341bf583f0c64
Logan <loganyuan6@gmail.com>
2026-02-16 00:43:53 -0500

    rolling testing implemented

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,5 @@
 from src.main import main
 
+# Run by calling "python -m src"
 if __name__ == "__main__":
     main("TSLA")
--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from src.plotters import *
 from src.predictors import predictor
 from src.simulator import simulator
 from src.analyzers import *
-from src.utils import trim_many
+from src.utils import *
 
 def main(symbol):
     # Define the variables
     data_start_date = "2023-01-01"
-    data_end_date = "2026-01-01"
+    data_end_date = "2026-02-01"
     analysis_start_date = "2024-01-01"
     data = data_fetcher(symbol, data_start_date, data_end_date)
     macd, macd_signal, macd_histogram = compute_macd(data)
@@ -51,27 +51,43 @@ def main(symbol):
     cci_plotter(symbol, cci)
 
     # Simulate using the buy sell signal
-    buy_sell_signal = predictor(data, macd, macd_signal, macd_histogram, cci, kdj_k, kdj_d, kdj_j, boll_mid, boll_lb, boll_ub)
-    print(buy_sell_signal.to_string())
+    #buy_sell_signal = predictor(data, macd, macd_signal, macd_histogram, cci, kdj_k, kdj_d, kdj_j, boll_mid, boll_lb, boll_ub)
+    #print(buy_sell_signal.to_string())
 
-    simulation_result = simulator(close, buy_sell_signal)
-    print(simulation_result.to_string())
+    #simulation_result = simulator(close, buy_sell_signal)
+    #print(simulation_result.to_string())
 
     # Indicator analysis
     # MACD
-    #macd_threshold_analyzer(close, macd, macd_histogram)
+    # temp_close = slice_period(close, "2025-01-01", "2025-06-01")
+    # temp_macd = slice_period(macd, "2025-01-01", "2025-06-01")
+    # temp_histogram = slice_period(macd_histogram, "2025-01-01", "2025-06-01")
+    # macd_threshold_analyzer(temp_close, temp_macd, temp_histogram)
 
-    result = macd_walkforward(
+    # MACD Backtest
+    # result = macd_walkforward(
+    #     close_prices=close,
+    #     macd=macd,
+    #     histogram=macd_histogram,
+    #     train_period=("2025-01-01", "2025-06-01"),
+    #     test_period=("2025-09-01", "2026-02-01")
+    # )
+    # print("Best params:", result["best_params"])
+    # print("Train return:", result["train_return_pct"])
+    # print("Test return:", result["test_return_pct"])
+
+    # MACD Rolling Backtest
+    results = macd_rolling_walkforward(
         close_prices=close,
         macd=macd,
         histogram=macd_histogram,
-        train_period=("2024-01-01", "2025-01-01"),
-        test_period=("2025-01-01", "2026-01-01")
+        train_window_months=12,
+        test_window_months=1
     )
+    print(results.head())
+    print("Avg test return:", results["test_return_pct"].mean())
+    results.to_csv("analysis/macd_rolling_walkforward.csv", index=False)
 
-    print("Best params:", result["best_params"])
-    print("Train return:", result["train_return_pct"])
-    print("Test return:", result["test_return_pct"])
-
+    # CCI
     #cci_threshold_analyzer(close, cci)
     return
